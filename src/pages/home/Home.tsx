ddce21085e927b4e041577c28634d099e6e4addf
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-08 09:14:12 -0300

    Commit com página home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+function Home() {
+    return (
+        <>
+            <div style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div>
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <h2>MercaDela!</h2>
+                        <p>Site em construção!</p>
+                    </div>
+
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <img 
+                            src="src\assets\MercaDela_Logo.jpg" 
+                            alt="Imagem da Página Home" 
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
