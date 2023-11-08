c1f49cae1fe120ac7d436cbd99b1da0bb122c486
Flávio Farias <34287888+oFurabio@users.noreply.github.com>
2023-11-08 17:13:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro";
 import { Contact } from "./pages/contact/Contact";
 import { Login } from "./pages/login/Login";
