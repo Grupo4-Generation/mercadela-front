f977d2833a2e13f191ef8f6c928fd10b6ac48d52
Lwppytta <amandalwppytta@gmail.com>
2023-11-13 18:25:32 -0300

    Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import Login from "./pages/Login/Login";
 import Contact from "./pages/contact/Contact";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Produto from "./pages/produto/Produto";
 import Home from "./pages/home/Home";
 import Sobre from "./pages/sobre/Sobre";
-
-import Home from './pages/home/Home';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Cadastro from './pages/cadastro/Cadastro';
-import { Contact } from './pages/contact/Contact';
-import Produto from './pages/produto/Produto';
-import { Login } from './pages/login/Login';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -26,12 +18,13 @@ function App() {
             <Navbar />
             <div className="flex-grow flex items-center justify-center my-5">
               <Routes>
+                <Route path="/home" element={<Home />} />
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/produto" element={<Produto />} />
-                <Route path="/home" element={<Home />} />
+                <Route path="/sobre" element={<Sobre />} />
               </Routes>
             </div>
             <Footer />
