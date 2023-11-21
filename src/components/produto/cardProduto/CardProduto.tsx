7142169fee29eb5aa70edeedb2c8215f9ff1bb4e
Flávio Farias <34287888+oFurabio@users.noreply.github.com>
2023-11-21 15:35:29 -0300

    Update CardProduto.tsx

--- src/components/produto/cardProduto/CardProduto.tsx
+++ src/components/produto/cardProduto/CardProduto.tsx
@@ -1,34 +1,28 @@
-import { Link } from "react-router-dom";
-import Produto from "../../../models/Produto";
+import React from "react";
 
 interface ProductCardProps {
-  produto: Produto;
+  nomeProduto: string;
+  precoProduto: string;
+  fotoProduto: string;
 }
 
-function CardProduto({ produto }: ProductCardProps) {
-
-  let precoFormatado = (produto.precoProduto / 1).toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  });
-
-  return (<>
-    <div className="max-w-xs rounded-lg p-3 shadow duration-150 hover:shadow-2xl">
-      <img className="w-full rounded-lg object-cover object-center bg-gray-100" src={produto.fotoProduto} alt="produto" />
+const CardProduto: React.FC<ProductCardProps> = ({ nomeProduto, precoProduto, fotoProduto }) => {
+  return (
+    <div className="max-w-xs rounded-lg p-3 shadow duration-150 hover:shadow-md">
+      <img className="w-full rounded-lg object-cover object-center bg-gray-100" src={fotoProduto} alt="product" />
       <div>
         <div className="my-3 flex items-center justify-center">
-          <p className="font-bold text-2xl text-orange-500">{produto.nomeProduto}</p>
+          <p className="font-bold text-2xl text-orange-500">{nomeProduto}</p>
         </div>
         <div className="my-2 flex justify-center">
-          <p className="text-xl font-semibold text-green-500">{precoFormatado}</p>
+          <p className="text-xl font-semibold text-green-500">{precoProduto}</p>
         </div>
         <div className="my-2 flex justify-center">
-          <Link to={`/produto/${produto.id}`}>
-            <p className="rounded-full cursor-pointer bg-green-500 px-2 text-4xl font-semibold hover:scale-105 text-white">Comprar</p>
-          </Link></div>
+          <p className="rounded-full cursor-pointer bg-green-500 px-2 text-4xl font-semibold hover:scale-105 text-white">Comprar</p>
+        </div>
       </div>
     </div>
-  </>);
+  );
 }
 
 export default CardProduto;
