d0d5a845b230ce03fcd8cdbf51a78d967e037b8a
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-21 15:58:17 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
+import CardProduto from "../../components/produto/cardProduto/CardProduto";
 import SlideHome from "../../components/slide/home/SlideHome";
-import CardHome from "./cardHome/CardHome";
 
 function Home() {
 
@@ -10,14 +10,13 @@ function Home() {
           <SlideHome />
         </div>
 
-        <h1 className="font-fontProjeto text-6xl text-[#DB5413] font-bold py-10 text-center">Produtos</h1>
+        <h1 className="text-3xl text-[#DB5413] font-bold pb-7 text-center">Produtos</h1>
 
-        <div className="flex space-x-3 items-center justify-between pb-20">
-          <CardHome nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
-          <CardHome nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
-          <CardHome nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
-          <CardHome nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
-          <CardHome nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
+        <div className="flex space-x-3 items-center justify-between">
+          <CardProduto nomeProduto="Mouse Logitech" precoProduto="R$ 199,90" fotoProduto="https://cdn.discordapp.com/attachments/1139577278892875784/1175053140085706792/Mouse_Logitech.png" />
+          <CardProduto nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
+          <CardProduto nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
+          <CardProduto nomeProduto="Pente" precoProduto="R$ 12,98" fotoProduto="src/assets/Pente.png" />
         </div>
       </div>
     </>
