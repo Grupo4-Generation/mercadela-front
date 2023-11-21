535b6d2c3238dac99cd5c78b9ad58a94e9460bfc
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-21 15:56:29 -0300

    Belez que q fakta

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
 
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormularioCategoria from "./components/categoria/formularioCategoria/FormularioCategoria";
+import ListaCategorias from "./components/categoria/listaCategoria/ListaCategoria";
 import Footer from "./components/footer/Footer";
-import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import Login from "./pages/Login/Login";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
 import Produto from "./pages/produto/Produto";
 import Sobre from "./pages/sobre/Sobre";
-import ListaCategorias from "./components/categoria/listaCategoria/ListaCategoria";
-import FormularioCategoria from "./components/categoria/formularioCategoria/FormularioCategoria";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/produto" element={<Produto />} />
+                <Route path="/produto/:id" element={<Produto />} />
                 <Route path="/sobre" element={<Sobre />} />
                 <Route path="/categoria" element={<ListaCategorias />} />
                 <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
