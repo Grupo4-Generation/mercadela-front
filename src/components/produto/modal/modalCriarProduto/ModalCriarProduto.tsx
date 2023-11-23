0951374f9f029220237ef977897877ef985f833b
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-23 12:26:09 -0300

    Fizemo cagada kkk

--- src/components/produto/modal/modalCriarProduto/ModalCriarProduto.tsx
+++ src/components/produto/modal/modalCriarProduto/ModalCriarProduto.tsx
@@ -1,7 +1,4 @@
-import EditarProduto from '../../crudProduto/editarProduto/EditarProduto';
-
 import '../modals.css';
-import Popup from 'reactjs-popup';
 
 function ModalCriarProduto(){
     return(
