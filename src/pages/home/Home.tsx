85688463862059047487c156ede3761f620d639b
Flávio Farias <34287888+oFurabio@users.noreply.github.com>
2024-03-18 20:01:48 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,10 +14,10 @@ function Home() {
         <h1 className="text-8xl text-[#DB5413] font-bold py-14 text-center">Produtos</h1>
 
         <div className="flex space-x-3 items-center justify-between">
-          <CardEstaticoProdutoHome nomeProduto="Apple Magic Mouse" precoProduto={499.90} fotoProduto="https://cdn.discordapp.com/attachments/1139577278892875785/1177254215467401258/mouse.png" />
-          <CardEstaticoProdutoHome nomeProduto="iPhone XR 256GB" precoProduto={1468.94} fotoProduto="http://cdn.discordapp.com/attachments/1139577278892875785/1177245722710655006/iphone.png" />
-          <CardEstaticoProdutoHome nomeProduto="Escova de Cabelo" precoProduto={14.98} fotoProduto="http://cdn.discordapp.com/attachments/1139577278892875784/1177225007064158290/Pente.png" />
-          <CardEstaticoProdutoHome nomeProduto="Frigideira Antiaderente" precoProduto={56.00} fotoProduto="http://cdn.discordapp.com/attachments/1139577278892875785/1177250791543799858/frigideira.png" />
+          <CardEstaticoProdutoHome nomeProduto="Apple Magic Mouse" precoProduto={499.90} fotoProduto="https://iili.io/JX18jbs.png" />
+          <CardEstaticoProdutoHome nomeProduto="iPhone XR 256GB" precoProduto={1468.94} fotoProduto="https://iili.io/JX18G0N.png" />
+          <CardEstaticoProdutoHome nomeProduto="Ursinho de Pelúcia" precoProduto={14.98} fotoProduto="https://iili.io/JX18LLx.png" />
+          <CardEstaticoProdutoHome nomeProduto="Frigideira Antiaderente" precoProduto={56.00} fotoProduto="https://iili.io/JX18IfV.png" />
         </div>
       </div>
     </>
