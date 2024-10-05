62b32aa46f089d78412f4fc6e56c68a6f0fdc121
Matheus Pereira da Silva <131720752+matheuxsx3@users.noreply.github.com>
2024-10-05 14:43:48 -0300

    Refactor routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,24 +25,24 @@ function App() {
     <>
       <div className="flex flex-col min-h-screen">
         <AuthProvider>
-        <ToastContainer/>
+          <ToastContainer />
           <BrowserRouter>
             <Navbar />
             <div className="flex flex-grow items-center justify-center mt-7">
               <Routes>
                 <Route path="/home" element={<Home />} />
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/product/:id" element={<SelectedProduct />} />
                 <Route path="/products" element={<ProductList />} />
                 <Route path="/about" element={<AboutUs />} />
-                <Route path="/createProduct" element={<EditProduct/>} />
+                <Route path="/createProduct" element={<EditProduct />} />
                 <Route path="/category" element={<CategoryList />} />
                 <Route path="/createCategory" element={<CategoryForm />} />
                 <Route path="/editCategory/:id" element={<CategoryForm />} />
-                <Route path="/loadProduct" element={< LoadProduto/>} />
+                <Route path="/loadProduct" element={<LoadProduto />} />
                 <Route path="/loadCategory" element={<CategoryLoad />} />
                 <Route path="/loadContact" element={<LoadContact />} />
               </Routes>
